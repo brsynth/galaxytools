b37f25ef4db129a20b58b87d7faa950402d6870b
guillaume-gricourt <guipagui@gmail.com>
2025-02-05 14:09:23 +0100

    fix(tools): molecule-signature, lint script

--- tools/molecule-signature/scripts/enumerate.py
+++ tools/molecule-signature/scripts/enumerate.py
@@ -1,34 +1,27 @@
 import argparse
 import logging
-import os
 import sys
 import time
 
 import pandas as pd
+from molsig.enumerate_signature import enumerate_molecule_from_morgan
+from molsig.SignatureAlphabet import load_alphabet
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from molsig.enumerate_signature import enumerate_molecule_from_morgan
-from molsig.SignatureAlphabet import load_alphabet, SignatureAlphabet
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--input-smiles-str", required=True, help="SMILES string"
-    )
-    parser.add_argument(
-        "--input-alphabet-npz", required=True, help="Alphabet file"
-    )
-    parser.add_argument(
-        "--output-data-tsv", required=True, help="Output file"
-    )
+    parser.add_argument("--input-smiles-str", required=True, help="SMILES string")
+    parser.add_argument("--input-alphabet-npz", required=True, help="Alphabet file")
+    parser.add_argument("--output-data-tsv", required=True, help="Output file")
     args = parser.parse_args()
 
     # Init
     logging.basicConfig(
         level=logging.DEBUG,  # Set the minimum log level (DEBUG, INFO, WARNING, etc.)
         format="%(asctime)s - %(levelname)s - %(message)s",  # Log format
-        handlers=[logging.StreamHandler()]  # Log to standard output
+        handlers=[logging.StreamHandler()],  # Log to standard output
     )
     logger = logging.getLogger(__name__)
 
@@ -43,12 +36,18 @@ if __name__ == "__main__":
     if mol is None:
         logging.error(f"SMILES is not validated by rdkit: {smiles}")
         sys.exit(1)
-    fpgen = AllChem.GetMorganGenerator(radius=Alphabet.radius, fpSize=Alphabet.nBits, includeChirality=Alphabet.use_stereo)
+    fpgen = AllChem.GetMorganGenerator(
+        radius=Alphabet.radius,
+        fpSize=Alphabet.nBits,
+        includeChirality=Alphabet.use_stereo,
+    )
     morgan = fpgen.GetCountFingerprint(mol).ToList()
 
     logging.info("Enumerate molecules - start")
     start = time.time()
-    Ssig, Smol, Nsig, thresholds_reached, computational_times = enumerate_molecule_from_morgan(morgan, Alphabet)
+    Ssig, Smol, Nsig, thresholds_reached, computational_times = (
+        enumerate_molecule_from_morgan(morgan, Alphabet)
+    )
     end = round(time.time() - start, 2)
     logging.info("Enumerate molecules - end")
     logging.info(f"Time computed: {end}")
--- tools/molecule-signature/scripts/signature.py
+++ tools/molecule-signature/scripts/signature.py
@@ -1,34 +1,34 @@
 import argparse
 import logging
-import os
 import sys
 
 import pandas as pd
-from rdkit import Chem
 from molsig.Signature import MoleculeSignature
+from rdkit import Chem
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--input-smiles-str", required=True, help="SMILES string"
-    )
-    parser.add_argument(
-        "--output-data-tsv", required=True, help="Output file"
-    )
+    parser.add_argument("--input-smiles-str", required=True, help="SMILES string")
+    parser.add_argument("--output-data-tsv", required=True, help="Output file")
     args = parser.parse_args()
 
     # Init
     logging.basicConfig(
         level=logging.DEBUG,  # Set the minimum log level (DEBUG, INFO, WARNING, etc.)
         format="%(asctime)s - %(levelname)s - %(message)s",  # Log format
-        handlers=[logging.StreamHandler()]  # Log to standard output
+        handlers=[logging.StreamHandler()],  # Log to standard output
     )
     logger = logging.getLogger(__name__)
 
     # Build signature
     logging.info("Load SMILES into rdkit")
-    mol = Chem.MolFromSmiles(args.input_smiles_str)
+    smiles = args.input_smiles_str
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        logging.error(f"SMILES is not validated by rdkit: {smiles}")
+        sys.exit(1)
+
     logging.info("Build signature")
     mol_sig = MoleculeSignature(mol)
 
