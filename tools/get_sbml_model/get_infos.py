fbecc3c90dca64f5233b5fc7ffdb8c8f1eb88a83
GuillaumeG <guipagui@gmail.com>
2025-03-20 09:51:14 +0100

    Fix tool `get_sbml_model` (#160)
    * fix(tools): get_sbml_model, rm unused argument
    
    * fix(tools): get_sbml_model, update cmdline and tests
    
    * chore(github): update
    
    * chore(github): add flake8 config
    
    * fix(tools): get_sbml_model, adjust input
    
    * fix(tools): get_sbml_model, output label
    
    * fix(tools): get_sbml_model, syntax
    
    * fix(tools): get_sbml_model, external data
    
    * fix(tools): get_sbml_model, syntax
    
    * fix(tools): get_sbml_model, syntax
    
    * fix(tools): get_sbml_model, syntax
    
    * fix(tools): get_sbml_model, syntax
    
    * fix(tools): get_sbml_model, syntax
    
    * fix(tools): get_sbml_model, syntax
    
    * fix(tools): get_sbml_model, syntax
    
    * fix(tools): get_sbml_model, try syntax
    
    * fix(tools): get_sbml_model, try syntax
    
    * fix(tools): get_sbml_model, try syntax
    
    * fix(tools): get_sbml_model, try syntax
    
    * fix(tools): get_sbml_model, try syntax
    
    * fix(tools): get_sbml_model, try syntax
    
    * fix(tools): get_sbml_model, id for GEMs

--- tools/get_sbml_model/get_infos.py
+++ tools/get_sbml_model/get_infos.py
@@ -1,83 +1,51 @@
 from argparse import ArgumentParser
-from libsbml import (
-    readSBMLFromFile
-)
+from libsbml import readSBMLFromFile
 from taxonid import get_taxonid
 from requests import get as r_get
 
 
 def get_biomass_rxn(sbml_doc):
-    '''
+    """
     Returns the biomass reaction of the model
-    
+
     Parameters
     ----------
     sbml_doc: libsbml.SBMLDocument
         SBML model
-        
+
     Returns
     -------
     biomass_rxn: libsbml.Reaction
         Biomass reaction
-    '''
+    """
     reactions = sbml_doc.getModel().getListOfReactions()
     # Search for 'biomass' keyword in reaction name
     for rxn in reactions:
-        if 'biomass' in rxn.getName().lower():
+        if "biomass" in rxn.getName().lower():
             return rxn
     # Search for 'biomass' keyword in products
     # AND not in reactants
     for rxn in reactions:
         in_reactants = False
         for reac in rxn.getListOfReactants():
-            if 'biomass' in reac.getSpecies().lower():
+            if "biomass" in reac.getSpecies().lower():
                 in_reactants = True
                 break
         if not in_reactants:
             for prod in rxn.getListOfProducts():
-                if 'biomass' in prod.getSpecies().lower():
+                if "biomass" in prod.getSpecies().lower():
                     return rxn
     return None
 
 
 def args():
-    parser = ArgumentParser('Returns cell informations')
-    parser.add_argument(
-        'infile',
-        type=str,
-        help='SBML input file (xml)'
-    )
-    # argument to tag file from BiGG
-    parser.add_argument(
-        '--bigg',
-        action='store_true',
-        help='Tag file from BiGG'
-    )
-    parser.add_argument(
-        '--comp',
-        type=str,
-        help='Path to store cell compartments'
-    )
-    parser.add_argument(
-        '--biomass',
-        type=str,
-        help='Path to store biomass reaction ID'
-    )
-    parser.add_argument(
-        '--biomass-id',
-        type=str,
-        help='ID of biomass reaction'
-    )
-    parser.add_argument(
-        '--hostname',
-        type=str,
-        help='Name of the host organism'
-    )
-    parser.add_argument(
-        '--taxid',
-        type=str,
-        help='Path to store host taxonomy ID'
-    )
+    parser = ArgumentParser("Returns cell informations")
+    parser.add_argument("infile", type=str, help="SBML input file (xml)")
+    parser.add_argument("--comp", type=str, help="Path to store cell compartments")
+    parser.add_argument("--biomass", type=str, help="Path to store biomass reaction ID")
+    parser.add_argument("--biomass-id", type=str, help="ID of biomass reaction")
+    parser.add_argument("--hostname", type=str, help="Name of the host organism")
+    parser.add_argument("--taxid", type=str, help="Path to store host taxonomy ID")
     params = parser.parse_args()
     return params
 
@@ -132,48 +100,47 @@ def get_taxon_id(hostid: str, bigg: bool):
 
 
 def entry_point():
-
     params = args()
 
     sbml_doc = readSBMLFromFile(params.infile)
 
     compartments = sbml_doc.getModel().getListOfCompartments()
-    comp_str = ''
+    comp_str = ""
     for comp in compartments:
-        comp_str += f'{comp.getId()}\t{comp.getName()}\n'
+        comp_str += f"{comp.getId()}\t{comp.getName()}\n"
     if params.comp:
-        with open(params.comp, 'w') as f:
-            f.write('#ID\tNAME\n')
+        with open(params.comp, "w") as f:
+            f.write("#ID\tNAME\n")
             f.write(comp_str)
     else:
-        print('Compartments:')
+        print("Compartments:")
         for comp in compartments:
-            print(f'{comp.getId()}\t{comp.getName()}'.replace('\n', ' | '))
+            print(f"{comp.getId()}\t{comp.getName()}".replace("\n", " | "))
 
     if params.biomass_id:
         biomass_rxn = sbml_doc.getModel().getReaction(params.biomass_id)
     else:
         biomass_rxn = get_biomass_rxn(sbml_doc)
     if not biomass_rxn:
-        print('Warning: unable to retrieve biomass reaction')
-        biomass_id = ''
+        print("Warning: unable to retrieve biomass reaction")
+        biomass_id = ""
     else:
         biomass_id = biomass_rxn.getId()
     if params.biomass:
-        with open(params.biomass, 'w') as f:
-            f.write('#ID\n')
-            f.write(f'{biomass_id}\n')
+        with open(params.biomass, "w") as f:
+            f.write("#ID\n")
+            f.write(f"{biomass_id}\n")
     else:
-        print(f'Biomass reaction ID: {biomass_id}')
+        print(f"Biomass reaction ID: {biomass_id}")
 
     taxid = get_taxon_id(params.hostname, params.bigg)
 
     if params.taxid:
-        with open(params.taxid, 'w') as f:
-            f.write('#ID\n')
-            f.write(f'{taxid}\n')
+        with open(params.taxid, "w") as f:
+            f.write("#ID\n")
+            f.write(f"{taxid}\n")
     else:
-        print(f'Taxonomy ID: {taxid}')
+        print(f"Taxonomy ID: {taxid}")
 
 
 if __name__ == "__main__":
