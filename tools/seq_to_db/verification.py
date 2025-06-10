3401816c949b538bd9c67e61cbe92badff6a4007
ramiz khaled <ramiz.khaled@inrae.fr>
2025-06-10 16:40:54 +0200

    update: dont activate container

---
+++ tools/seq_to_db/verification.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, text
+
+db_uri = "postgresql://postgres:RK17@localhost:5432/test_fragments_db"  # adapt with your URI's DB
+engine = create_engine(db_uri)
+
+with engine.connect() as conn:
+    result = conn.execute(text("""
+        SELECT fragment, sequence, annotation
+        FROM sample
+        ORDER BY fragment
+    """))
+
+    print("Full contents of fragments in DB:\n")
+    for row in result:
+        print(f" Fragment: {row.fragment}")
+        print(" Sequence:")
+        print(row.sequence)
+        print("\n Annotation:")
+        print(row.annotation)
+        print("-" * 80)
